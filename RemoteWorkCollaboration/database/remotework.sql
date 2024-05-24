0b21ad699f7a82710f907ca0b0328bc72f02ff30
niyigenamike <122165074+niyigenamike@users.noreply.github.com>
2024-05-24 00:43:50 -0700

    Add files via upload

---
+++ RemoteWorkCollaboration/database/remotework.sql
@@ -0,0 +1,407 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 24, 2024 at 07:40 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 7.4.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `remotework`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `auditor`
+--
+
+CREATE TABLE `auditor` (
+  `id` int(11) NOT NULL,
+  `fullnames` varchar(255) NOT NULL,
+  `phone` varchar(50) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `address` text NOT NULL,
+  `gender` varchar(10) NOT NULL,
+  `status` varchar(50) NOT NULL,
+  `age` int(11) NOT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  `date_` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `auditor`
+--
+
+INSERT INTO `auditor` (`id`, `fullnames`, `phone`, `email`, `address`, `gender`, `status`, `age`, `image`, `date_`) VALUES
+(1, 'umutima', '34567', 'niyigenamike4@gmail.com', 'huye', 'male', 'active', 90, '', '2024-05-22 10:46:37');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chats`
+--
+
+CREATE TABLE `chats` (
+  `id` int(11) NOT NULL,
+  `message` text NOT NULL,
+  `sender` varchar(255) NOT NULL,
+  `email` varchar(90) NOT NULL,
+  `timestamp` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `chats`
+--
+
+INSERT INTO `chats` (`id`, `message`, `sender`, `email`, `timestamp`) VALUES
+(1, '[value-1]', '1', '[value-3]', '2024-05-22 13:59:37'),
+(2, '[value-1]', '2', '[value-3]', '2024-05-22 14:31:04'),
+(3, 'this is me', '1', '', '2024-05-22 14:32:50'),
+(4, 'and me also', '1', 'ciri@gmail.com', '2024-05-22 14:34:55'),
+(5, 'this is the chriss and me ', '1', 'ciri@gmail.com', '2024-05-22 16:05:00'),
+(6, 'hiii', '1', 'ciri@gmail.com', '2024-05-24 08:29:27'),
+(7, 'kendra', '1', 'ciri@gmail.com', '2024-05-24 08:29:45'),
+(8, 'this', '1', 'ciri@gmail.com', '2024-05-24 08:33:22');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `location`
+--
+
+CREATE TABLE `location` (
+  `id` int(11) NOT NULL,
+  `location_name` varchar(255) NOT NULL,
+  `location_number` varchar(50) NOT NULL,
+  `district` varchar(255) NOT NULL,
+  `sector` varchar(255) NOT NULL,
+  `cell` varchar(255) NOT NULL,
+  `village` varchar(255) NOT NULL,
+  `date_added` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `projects`
+--
+
+CREATE TABLE `projects` (
+  `id` int(11) NOT NULL,
+  `project_name` varchar(255) NOT NULL,
+  `project_description` text NOT NULL,
+  `projectImage` varchar(255) NOT NULL,
+  `start_date` date NOT NULL,
+  `end_date` date NOT NULL,
+  `budget` decimal(10,2) NOT NULL,
+  `dateCreated` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `projects`
+--
+
+INSERT INTO `projects` (`id`, `project_name`, `project_description`, `projectImage`, `start_date`, `end_date`, `budget`, `dateCreated`) VALUES
+(1, 'murara', 'qw', 'uploads/Capture nnha - Copy.PNG', '0000-00-00', '0000-00-00', 0.00, '2024-05-22 10:58:12');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `supervisor`
+--
+
+CREATE TABLE `supervisor` (
+  `id` int(11) NOT NULL,
+  `fullnames` varchar(255) NOT NULL,
+  `phone` varchar(50) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `address` text NOT NULL,
+  `gender` varchar(10) NOT NULL,
+  `status` varchar(50) NOT NULL,
+  `age` int(11) NOT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  `date_` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `supervisor`
+--
+
+INSERT INTO `supervisor` (`id`, `fullnames`, `phone`, `email`, `address`, `gender`, `status`, `age`, `image`, `date_`) VALUES
+(1, 'para', '34567', 'niyigenamike4@gmail.com', 'huye', 'male', 'acrive', 88, 'uploads/avatar.PNG', '2024-05-22 10:48:49');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `system_log_info`
+--
+
+CREATE TABLE `system_log_info` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `action` text NOT NULL,
+  `timestamp` datetime NOT NULL DEFAULT current_timestamp(),
+  `ip_address` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `task`
+--
+
+CREATE TABLE `task` (
+  `id` int(11) NOT NULL,
+  `task_name` varchar(255) NOT NULL,
+  `taskDescription` text NOT NULL,
+  `tool_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `task`
+--
+
+INSERT INTO `task` (`id`, `task_name`, `taskDescription`, `tool_name`) VALUES
+(1, 'image', 'sdsds', 'camera 3');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `team`
+--
+
+CREATE TABLE `team` (
+  `id` int(11) NOT NULL,
+  `team_name` varchar(255) NOT NULL,
+  `team_description` text NOT NULL,
+  `teamImage` varchar(255) DEFAULT NULL,
+  `dateAdded` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `team`
+--
+
+INSERT INTO `team` (`id`, `team_name`, `team_description`, `teamImage`, `dateAdded`) VALUES
+(1, 'ciri', 'we', 'uploads/BIT.png', '2024-05-22 11:00:19');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tools`
+--
+
+CREATE TABLE `tools` (
+  `id` int(11) NOT NULL,
+  `tool_name` varchar(255) NOT NULL,
+  `toolDescription` text NOT NULL,
+  `tool_image` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tools`
+--
+
+INSERT INTO `tools` (`id`, `tool_name`, `toolDescription`, `tool_image`) VALUES
+(1, 'camera 3', 'this is it', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `gender` varchar(90) NOT NULL,
+  `contact` varchar(90) NOT NULL,
+  `role` varchar(50) NOT NULL,
+  `status` varchar(50) NOT NULL,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `first_name`, `last_name`, `username`, `password`, `email`, `gender`, `contact`, `role`, `status`, `date_created`) VALUES
+(1, 'ciri@gmail.com', 'ciri@gmail.com', '', 'd1457b321ab5fc1260de7ac0c50de28a', 'ciri@gmail.com', 'Male', '123', 'Student', '', '2024-05-22 12:42:58'),
+(2, 'photographer', 'photographer', '', '4736bc65011f7b2432fb77676b8bb3d5', 'photographer@gmail.com', 'Male', '98809898098809780', 'Photographer', '', '2024-05-24 09:23:10'),
+(3, 'admin', 'admin', '', '75d23af433e0cea4c0e45a56dba18b30', 'admin@gmail.com', 'Male', '123', 'Admin', '', '2024-05-24 09:24:03');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `workdoneonproject`
+--
+
+CREATE TABLE `workdoneonproject` (
+  `id` int(11) NOT NULL,
+  `project_name` varchar(90) DEFAULT NULL,
+  `project_id` varchar(90) DEFAULT NULL,
+  `sharedImage` varchar(90) DEFAULT NULL,
+  `userId` int(11) DEFAULT NULL,
+  `daetAdded` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `auditor`
+--
+ALTER TABLE `auditor`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `chats`
+--
+ALTER TABLE `chats`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `location`
+--
+ALTER TABLE `location`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `projects`
+--
+ALTER TABLE `projects`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `supervisor`
+--
+ALTER TABLE `supervisor`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `system_log_info`
+--
+ALTER TABLE `system_log_info`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `task`
+--
+ALTER TABLE `task`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `team`
+--
+ALTER TABLE `team`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tools`
+--
+ALTER TABLE `tools`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `workdoneonproject`
+--
+ALTER TABLE `workdoneonproject`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `auditor`
+--
+ALTER TABLE `auditor`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `chats`
+--
+ALTER TABLE `chats`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `location`
+--
+ALTER TABLE `location`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `projects`
+--
+ALTER TABLE `projects`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `supervisor`
+--
+ALTER TABLE `supervisor`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `system_log_info`
+--
+ALTER TABLE `system_log_info`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `task`
+--
+ALTER TABLE `task`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `team`
+--
+ALTER TABLE `team`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tools`
+--
+ALTER TABLE `tools`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `workdoneonproject`
+--
+ALTER TABLE `workdoneonproject`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

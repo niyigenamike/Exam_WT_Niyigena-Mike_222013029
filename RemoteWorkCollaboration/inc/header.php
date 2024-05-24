5fa7aaef5299120f40990243734f0d98a357fcdc
niyigenamike <122165074+niyigenamike@users.noreply.github.com>
2024-05-24 00:36:56 -0700

    Add files via upload

---
+++ RemoteWorkCollaboration/inc/header.php
@@ -0,0 +1,39 @@
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  	<title><?php echo  "PhotoGrapher" ?></title>
+    <link rel="stylesheet" href="cssAndJs/adminlte.css">
+     <link rel="stylesheet" href="cssAndJs/styles.css">
+     <link rel="stylesheet" href="cssAndJs/bootstrap-4.min.css">
+     <script src="cssAndJs\sweetalert2.min.css"></script>
+
+     <!-- jQuery -->
+    <script src="cssAndJs\jquery.min.js"></script>
+     <!-- Toastr -->
+     <script src="cssAndJs\toastr.min.js"></script>
+     <script src="cssAndJs\sweetalert2.min.js"></script>
+
+    <script src="dist/js/script.js"></script>
+    <style>
+    #main-header{
+        position:relative;
+        background: rgb(0,0,0)!important;
+        background: radial-gradient(circle, rgba(0,0,0,0.48503151260504207) 22%, rgba(0,0,0,0.39539565826330536) 49%, rgba(0,212,255,0) 100%)!important;
+    }
+    #main-header:before{
+        content:"";
+        position:absolute;
+        top:0;
+        left:0;
+        width:100%;
+        height:100%;
+        background-image:url("uploads/1626397620_books.jpg");
+        background-repeat: no-repeat;
+        background-size: cover;
+        filter: drop-shadow(0px 7px 6px black);
+        z-index:-1;
+    }
+
+ </style>
+  </head>
